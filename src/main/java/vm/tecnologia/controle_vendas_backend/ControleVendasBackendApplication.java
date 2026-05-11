dcfc47c7af2ff197c75b089b0c5eae639a62939c
Tiago Rodrigues <tirodrigues7@hotmail.com>
2026-05-11 11:15:35 -0300

    projeto inicial

---
+++ src/main/java/vm/tecnologia/controle_vendas_backend/ControleVendasBackendApplication.java
@@ -0,0 +1,13 @@
+package vm.tecnologia.controle_vendas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleVendasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleVendasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vm/tecnologia/controle_vendas_backend/ControleVendasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vm.tecnologia.controle_vendas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleVendasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
